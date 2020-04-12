8c826c80ce5c7f8cd3bcbbab01dbc3f04194ff41
robertarfa <roberta_rfa@hotmail.com>
2020-04-12 19:02:22 -0300

    criado pastas dos modulos

--- Works/aulajs/desafio1-3-1.js
+++
@@ -1,9 +0,0 @@
-const usuarios = [
-    { nome: "Roberta", tecnologias: ["HTML", "CSS", "JavaScript"] },
-    { nome: "Vanessa", tecnologias: ["Java", "C#"] },
-    { nome: "Renata", tecnologias: ["React", "Node.js"] }
-  ];
-
-  for (let usuario of usuarios) {
-    console.log(`${usuario.nome} trabalha com ${usuario.tecnologias.join(', ')}`)
-}
