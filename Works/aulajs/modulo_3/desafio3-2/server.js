3e43d2a5d383fb55b8aeae95518edb0de918dddf
robertarfa <roberta_rfa@hotmail.com>
2020-04-17 20:13:39 -0300

    atualização dinâmica de todas as informações

--- Works/aulajs/modulo_3/desafio3-2/server.js
+++ Works/aulajs/modulo_3/desafio3-2/server.js
@@ -10,12 +10,27 @@ server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
     express: server,
-    noCache: true
+    noCache: true,
+    autoescape: false
 })
 
 //routes
 server.get('/', function(req, res){
-    return res.render('about')
+    const about = {
+        avatar_url: "https://avatars2.githubusercontent.com/u/22533625?s=400&u=2ea5d05adbc2847fbeeecd7358bbecedcc431d6a&v=4",
+        name: "Roberta Aguilar",
+        role: "Estudante de HTML, CSS e JavaScript",
+        description1: "Continuamente trabalhando para melhorar as habilidades em HTML, CSS e JavaScript.",
+        description2: 'Atualmente estudante do bootcamp LaunchBase da <a href="https://rocketseat.com.br/" target="blank">Rocketseat</a>',
+        link: [
+            {name:"Linkedin", url: "https://www.linkedin.com/in/robertarfa/"},
+            {name:"GitHub", url:"https://github.com/robertarfa"},
+            {name:"Sourcerer" , url:"https://sourcerer.io/robertarfa"},
+            {name:"Twiter" , url: "https://twitter.com/roberta_rfa"}
+        ]
+
+    }
+    return res.render('about', {about})
 })
 server.get('/aulas', function(req, res){
     return res.render('aulas', {items: videos})
