0c69b78b874fbfba66271f8ce08a47c04b87eef6
robertarfa <roberta_rfa@hotmail.com>
2020-04-17 19:09:11 -0300

    desafio 3-1

---
+++ Works/aulajs/modulo_3/desafio3-1/data.js
@@ -0,0 +1,20 @@
+module.exports =[
+    {
+        id: "c7P03kkrEG8",
+        title: "Configurando o VScode",
+        duration: "22 min",
+
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS GRID na prática",
+        duration: "36 min",
+
+    },
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário animado com JS e CSS",
+        duration: "57 min",
+
+    },
+]
